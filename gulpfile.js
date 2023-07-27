a3ec3a70710501bb7e7c192f4a9b86eb96721319
Theedon <faniyantoyin@gmail.com>
2023-07-27 15:07:45 +0100

    correct gulp file to see files correctly

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const { src, dest, series, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 const buildStyles = () => {
-  return src("./shinobi/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return src("./getdonny/**/*.scss").pipe(sass()).pipe(dest("css"));
 };
 
 const watchTask = () => {
-  watch(["./shinobi/**/*.scss"], buildStyles);
+  watch(["./getdonny/**/*.scss"], buildStyles);
 };
 
 exports.default = series(buildStyles, watchTask);
